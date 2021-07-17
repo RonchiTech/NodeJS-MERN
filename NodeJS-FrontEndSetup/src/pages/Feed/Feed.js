017184e76368fe3151a1d57948bb99da39ea9e88
ronchitech <ronchibusiness@gmail.com>
2021-07-17 19:25:28 +0800

    setting up status routes and controllers, getting and updating status from back to front

--- NodeJS-FrontEndSetup/src/pages/Feed/Feed.js
+++ NodeJS-FrontEndSetup/src/pages/Feed/Feed.js
@@ -22,7 +22,11 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -50,10 +54,10 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/api/posts?page='+page, {
+    fetch('http://localhost:8080/api/posts?page=' + page, {
       headers: {
-        Authorization: `Bearer ${this.props.token}`
-      }
+        Authorization: `Bearer ${this.props.token}`,
+      },
     })
       .then((res) => {
         if (res.status !== 200) {
@@ -79,7 +83,16 @@ class Feed extends Component {
 
   statusUpdateHandler = (event) => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.props.token}`,
+      },
+      body: JSON.stringify({
+        status: this.state.status,
+      }),
+    })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
@@ -130,8 +143,8 @@ class Feed extends Component {
       method: method,
       body: formData,
       headers: {
-        Authorization: `Bearer ${this.props.token}`
-      }
+        Authorization: `Bearer ${this.props.token}`,
+      },
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
